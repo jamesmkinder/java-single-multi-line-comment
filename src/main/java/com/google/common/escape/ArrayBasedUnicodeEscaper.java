ad342f1d2012d0d5dae4b4a043a4fe12a6fcf75e
Ozge13 <ozge.ozdes@avisi.nl>
2022-03-21 21:28:02 +0000

    Update at 2022-03-21T21:28:02.069206Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -1,16 +1,16 @@
 package com.google.common.escape;
 
 public abstract class ArrayBasedUnicodeEscaper {
-    // The first code point in the safe range.
+    /* The first code point in the safe range. */
     private int safeMin;
-    // The last code point in the safe range.
+    /* The last code point in the safe range. */
     private int safeMax;
 
     private char safeMinChar;
     private char safeMaxChar;
 
     protected ArrayBasedUnicodeEscaper() {
-        /* 
+        /*
         This is a bit of a hack but lets us do quicker per-character checks in
          the fast path code. The safe min/max values are very unlikely to extend
          into the range of surrogate characters, but if they do we must not test
@@ -40,7 +40,7 @@ public abstract class ArrayBasedUnicodeEscaper {
         return s;
     }
 
-    /* Overridden for performance. */
+    // Overridden for performance.
     protected final int nextEscapeIndex(CharSequence csq, int index, int end) {
         return index;
     }
