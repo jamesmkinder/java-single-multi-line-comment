b2c40fdf5a6f998354b0c4d72c7c8459f9b2e744
yunghf <yung.hf@outlook.com>
2022-03-22 05:45:59 +0000

    Update at 2022-03-22T05:45:59.206733Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -3,7 +3,7 @@ package com.google.common.escape;
 public abstract class ArrayBasedUnicodeEscaper {
     /* The first code point in the safe range. */
     private int safeMin;
-    /* The last code point in the safe range.*/
+    /* The last code point in the safe range. */
     private int safeMax;
 
     private char safeMinChar;
