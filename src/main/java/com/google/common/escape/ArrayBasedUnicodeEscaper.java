e63fb79872d4f20291d1fb7794df0e3e72108c34
yunghf <yung.hf@outlook.com>
2022-03-22 05:43:54 +0000

    Update at 2022-03-22T05:43:54.218870Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -1,5 +1,4 @@
 package com.google.common.escape;
-package com.google.common.escape;
 
 public abstract class ArrayBasedUnicodeEscaper {
     /* The first code point in the safe range. */
@@ -12,20 +11,20 @@ public abstract class ArrayBasedUnicodeEscaper {
 
     protected ArrayBasedUnicodeEscaper() {
         /*
-        This is a bit of a hack but lets us do quicker per-character checks in
-        the fast path code. The safe min/max values are very unlikely to extend
-        into the range of surrogate characters, but if they do we must not test
-        any values in that range. To see why, consider the case where:
-        safeMin <= {hi,lo} <= safeMax
-        where {hi,lo} are characters forming a surrogate pair such that:
-        codePointOf(hi, lo) > safeMax
-        which would result in the surrogate pair being (wrongly) considered safe.
-        If we clip the safe range used during the per-character tests so it is
-        below the values of characters in surrogate pairs, this cannot occur.
-        This approach does mean that we break out of the fast path code in cases
-        where we don't strictly need to, but this situation will almost never
-        occur in practice.
-        */
+         * This is a bit of a hack but lets us do quicker per-character checks in
+         * the fast path code. The safe min/max values are very unlikely to extend
+         * into the range of surrogate characters, but if they do we must not test
+         * any values in that range. To see why, consider the case where:
+         * safeMin <= {hi,lo} <= safeMax
+         * where {hi,lo} are characters forming a surrogate pair such that:
+         * codePointOf(hi, lo) > safeMax
+         * which would result in the surrogate pair being (wrongly) considered safe.
+         * If we clip the safe range used during the per-character tests so it is
+         * below the values of characters in surrogate pairs, this cannot occur.
+         * This approach does mean that we break out of the fast path code in cases
+         * where we don't strictly need to, but this situation will almost never
+         * occur in practice.
+         */
         if (safeMin >= Character.MIN_HIGH_SURROGATE) {
             this.safeMinChar = Character.MAX_VALUE;
             this.safeMaxChar = 0;
@@ -33,12 +32,10 @@ public abstract class ArrayBasedUnicodeEscaper {
             this.safeMinChar = (char) safeMin;
             this.safeMaxChar = (char) Math.min(safeMax, Character.MIN_HIGH_SURROGATE - 1);
         }
-    
+    }
 
-    
-    // This is overridden to improve performance. Rough benchmarking shows that this almost doubles
-    // the speed when processing strings that do not require any escaping.
-    
+     // This is overridden to improve performance. Rough benchmarking shows that this almost doubles
+     // the speed when processing strings that do not require any escaping.
     public final String escape(String s) {
         return s;
     }
