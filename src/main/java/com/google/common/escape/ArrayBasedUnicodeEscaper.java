884649855d599b669ecbcac2203dec622d8232d9
Azhigit <Azgithub@163.com>
2022-04-18 03:39:36 +0000

    Update at 2022-04-18T03:39:36.617275Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -10,7 +10,8 @@ public abstract class ArrayBasedUnicodeEscaper {
     private char safeMaxChar;
 
     protected ArrayBasedUnicodeEscaper() {
-        /*// This is a bit of a hack but lets us do quicker per-character checks in
+        /*
+        // This is a bit of a hack but lets us do quicker per-character checks in
         // the fast path code. The safe min/max values are very unlikely to extend
         // into the range of surrogate characters, but if they do we must not test
         // any values in that range. To see why, consider the case where:
@@ -33,15 +34,15 @@ public abstract class ArrayBasedUnicodeEscaper {
         }
     }
 
-    ///*
+    /*
     // * This is overridden to improve performance. Rough benchmarking shows that this almost doubles
     //* the speed when processing strings that do not require any escaping.
-    /// */
+    / */
     public final String escape(String s) {
         return s;
     }
 
-    ///* Overridden for performance. */
+    //* Overridden for performance. */
     protected final int nextEscapeIndex(CharSequence csq, int index, int end) {
         return index;
     }
