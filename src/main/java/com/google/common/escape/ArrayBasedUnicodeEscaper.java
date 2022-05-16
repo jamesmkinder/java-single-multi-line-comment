1bc63418fef0d40549c84e1a97e3ae6951be2384
jamesmkinder <jmkgfk@gmail.com>
2022-05-16 03:29:47 +0000

    Update at 2022-05-16T03:29:47.166386Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -3,7 +3,9 @@ package com.google.common.escape;
 public abstract class ArrayBasedUnicodeEscaper {
     // The first code point in the safe range.
     private int safeMin;
-    // The last code point in the safe range.
+    /*
+    The last code point in the safe range.
+    */
     private int safeMax;
 
     private char safeMinChar;
@@ -32,10 +34,10 @@ public abstract class ArrayBasedUnicodeEscaper {
         }
     }
 
-    /*
-     * This is overridden to improve performance. Rough benchmarking shows that this almost doubles
-     * the speed when processing strings that do not require any escaping.
-     */
+    
+    //This is overridden to improve performance. Rough benchmarking shows that this almost doubles
+    //the speed when processing strings that do not require any escaping.
+     
     public final String escape(String s) {
         return s;
     }
